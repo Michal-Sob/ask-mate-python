ece8e56cf00873172ea840ef97743a97fac68f3c
baginski95 <bagiczny3@gmail.com>
2020-02-19 11:17:09 +0100

    Create connect_question_with_his_answer
    This function allows to conect a questions with all his answers and show it at question page

--- data_manager.py
+++ data_manager.py
@@ -15,6 +15,14 @@ def get_questions():
     return connection.import_data(QUESTIONS_FILE_PATH)
 
 
+def connect_question_with_his_answer(id):
+    selected_question = [question for question in get_questions() if question['id'] == id]
+    for answer in get_answers():
+        if id == answer['question_id']:
+            selected_question.append(answer)
+    return selected_question
+
+
 def submisson_time():
     now = datetime.now()
     timestamp = round(datetime.timestamp(now))
