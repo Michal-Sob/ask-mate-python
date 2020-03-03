382cd8b30e28b64137ebedee6f6f9532eb30a19c
baginski95 <bagiczny3@gmail.com>
2020-03-03 13:21:33 +0100

    Delete unnecessery functions

--- data_manager.py
+++ data_manager.py
@@ -20,35 +20,6 @@ def get_questions(cursor, question_id=None):
     return questions
 
 
-def connect_question_with_his_answer(id):
-    selected_question = [question for question in get_questions() if question['id'] == id]
-    for answer in get_answers():
-        if id == answer['question_id']:
-            selected_question.append(answer)
-    return selected_question
-
-
-def new_question_data():
-    new_id = max_id(get_questions()) + 1
-    data = {
-        "id": str(new_id),
-        "submission_time": str(submission_time()),
-        "view_number": "0",
-        "vote_number": "0"
-    }
-    return data
-
-
-def new_question_manager(new_question):
-    new_question.update(new_question_data())
-    fieldnames = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-    connection.export_data(QUESTIONS_FILE_PATH, fieldnames, new_question)
-
-    new_question_id = max_id(get_questions())
-
-    return new_question_id
-
-
 @connection.connection_handler
 def new_question_manager(cursor, new_question):
     cursor.execute("""INSERT INTO question (title,submission_time, message) VALUES (%s,%s,%s);""", (new_question['title'], util.submission_time(), new_question['message'],))
