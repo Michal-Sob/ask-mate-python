946b285282bcb6c2b19ff92a0856f7201231a6d0
baginski95 <bagiczny3@gmail.com>
2020-03-16 13:15:19 +0100

    Imported bcrypt and added functions which  add salt ,encode and hash passwords

--- util.py
+++ util.py
@@ -1,6 +1,19 @@
 from datetime import datetime
+import bcrypt
+
 
 def submission_time():
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     return now
+
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
