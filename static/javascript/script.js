45f32dd70d898ff09a291795faa08b2d9336b35f
Michal Nowak <michalkacpernowak@gmail.com>
2020-03-18 19:59:33 +0100

    BUGFIX login, sign in button CSS update registration update
    login.html - changed 'passsword' to 'password' ; deleted 'id' from label
    
    registration_form.html - CSS button update ; updated required pattern in password
    
    layout.html - sign in button fix

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,6 +1,16 @@
 function showPassword() {
 
     let x = document.getElementById("password");
+    let span = document.getElementById("checkbox_text");
+
+
+    if (span.innerHTML === "Hide") {
+        span.innerHTML = "Show"
+    }
+    else {
+        span.innerHTML = "Hide"
+    }
+
 
     if (x.type === "password") {
     x.type = "text";
